32738b804b077888bc98007bb452fd0f1354092f
Josiah Campbell <josiah.campbell@inin.com>
2017-10-03 20:18:12 -0400

    fixing implementation for consuming apps

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # ember-dc-shim
 
-```
-import d3 from 'ember-dc';
-import crossfilter from 'ember-dc';
-import dc from 'ember-dc';
+Shim for making [dc.js](http://dc-js.github.io/dc.js/), [d3.js](https://d3js.org/), [crossfilter](http://crossfilter.github.io/crossfilter/), and [d3-tip](http://labratrevenge.com/d3-tip/) available in your ember apps as es6 modules
+
+```javascript
+import d3 from 'd3';
+import crossfilter from 'crossfilter';
+import dc from 'dc';
 ```
 
 ## Installation
--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const mergeTrees = require('broccoli-merge-trees');
 const funnel = require('broccoli-funnel');
 const path = require('path');
 
+let d3Node;
+let d3TipNode;
+let dcNode;
+let crossfilterNode;
+
 module.exports = {
   name: 'ember-dc',
 
   included() {
     this._super(...arguments);
-    this.d3Node = new UnwatchedDir(path.dirname(require.resolve('d3')));
-    this.d3TipNode = new UnwatchedDir(path.dirname(require.resolve('d3-tip')));
-    this.dcNode = new UnwatchedDir(path.dirname(require.resolve('dc')));
-    this.crossfilterNode = new UnwatchedDir(path.dirname(require.resolve('crossfilter')));
+    d3Node = new UnwatchedDir(path.dirname(require.resolve('d3')));
+    d3TipNode = new UnwatchedDir(path.dirname(require.resolve('d3-tip')));
+    dcNode = new UnwatchedDir(path.dirname(require.resolve('dc')));
+    crossfilterNode = new UnwatchedDir(path.dirname(require.resolve('crossfilter')));
     this.importDependencies();
   },
 
@@ -24,7 +29,7 @@ module.exports = {
         development: 'vendor/d3/d3.js',
         production: 'vendor/d3/d3.min.js'
       },
-      { 
+      {
         prepend: true
       }
     );
@@ -46,9 +51,9 @@ module.exports = {
       }
     );
 
-    this.import('vendor/d3-shim.js');
-    this.import('vendor/dc-shim.js');
-    this.import('vendor/crossfilter-shim.js');
+    this.import('vendor/shims/d3-shim.js');
+    this.import('vendor/shims/dc-shim.js');
+    this.import('vendor/shims/crossfilter-shim.js');
     this.import('vendor/d3-tip/index.js');
   },
 
@@ -56,28 +61,35 @@ module.exports = {
     let trees = [];
 
     trees.push(
-      funnel(this.d3Node, {
+      funnel(path.join(__dirname, 'vendor', 'shims'), {
+        destDir: path.join('shims'),
+        files: ['dc-shim.js', 'd3-shim.js', 'crossfilter-shim.js']
+      })
+    );
+
+    trees.push(
+      funnel(d3Node, {
         destDir: 'd3',
         files: ['d3.js', 'd3.min.js']
       })
     );
 
     trees.push(
-      funnel(this.d3TipNode, {
+      funnel(d3TipNode, {
         destDir: 'd3-tip',
         files: ['index.js']
       })
     );
 
     trees.push(
-      funnel(this.dcNode, {
+      funnel(dcNode, {
         destDir: 'dc',
         files: ['dc.js', 'dc.min.js', 'dc.js.map']
       })
     );
 
     trees.push(
-      funnel(this.crossfilterNode, {
+      funnel(crossfilterNode, {
         destDir: 'crossfilter',
         files: ['crossfilter.js', 'crossfilter.min.js']
       })
